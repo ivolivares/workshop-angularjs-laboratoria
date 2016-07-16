ddb7d2c50057f9ed81398582783300e2d320ce5c
Iván Olivares R <hola@ivolivares.cl>
2016-07-16 12:00:54 -0300

    My Directives :smile:

--- Examples/2.Directives/example2/app.js
+++ Examples/2.Directives/example2/app.js
@@ -1,29 +1,17 @@
 var mainApp = angular.module("mainApp", []);
 
-mainApp.directive('fotoRandom', function() {
-  var directive = {
-    restrict: 'AE',
-    template: '<img src="">',
-    replace: true
+mainApp.directive('laboratoria', function() {
+  return {
+      restrict: 'AE',
+      replace: 'true',
+      template: '<h1>Esto es Laboratoria!!</h1>'
   };
-  
-  directive.scope = {
-     size : "=size"
-  };
-  
-  directive.compile = function(element, attributes) {
-     var linkFunction = function($scope, element, attributes) {
-        var randomString = Math.random().toString(36).substring(7),
-            size = (attrs.size) ? attrs.size : 250;
-        
-        scope.img = 'https://api.adorable.io/avatars/' + size + '/'+ randomString;
-     }
-     return linkFunction;
-  };
-  
-  return directive;
 });
-         
-mainApp.controller('applicationController', function($scope) {
-  $scope.sizes = [500, 300, 200, 100];
+
+mainApp.directive('laboratoriaAngular', function() {
+  return {
+      restrict: 'AE',
+      replace: 'true',
+      template: '<h1>Esto es AngularJS en Laboratoria!!</h1>'
+  };
 });
