9bb2eb2c2191eb898cd187deb9ed30364caea797
Iván Olivares R <hola@ivolivares.cl>
2016-07-18 01:07:05 -0300

    Forms and Validations :smile:

---
+++ Examples/7.FormsAndValidations/js/studentController.js
@@ -0,0 +1,19 @@
+'use strict';
+angular.module('mainApp', [])
+  .controller('studentController', function($scope) {
+    $scope.master = {};
+
+    $scope.update = function(user) {
+      $scope.master = angular.copy(user);
+    };
+
+    $scope.reset = function(form) {
+      if (form) {
+        form.$setPristine();
+        form.$setUntouched();
+      }
+      $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.reset();
+  });
