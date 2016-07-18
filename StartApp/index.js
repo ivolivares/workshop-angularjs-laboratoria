329833befceba6fa165cabcd93dffeebc3a4a7ff
Iván Olivares R <hola@ivolivares.cl>
2016-07-18 01:13:12 -0300

    Delete StartApp temporarily

--- StartApp/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('./public'));
-
-app.listen(3000, function() {
-  console.log('Application running on http://localhost:3000/index.html');
-});
